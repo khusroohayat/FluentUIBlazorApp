a8b68ef793923552acac15eaac7d783be0bb5b21
khusroo hayat <khusroo.hayat@gmail.com>
2024-09-04 11:04:06 +0500

    Add project files.

---
+++ FluentUIBlazorApp/FluentUIBlazorApp/Program.cs
@@ -0,0 +1,33 @@
+using FluentUIBlazorApp.Client.Pages;
+using FluentUIBlazorApp.Components;
+using Microsoft.FluentUI.AspNetCore.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+builder.Services.AddFluentUIComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(FluentUIBlazorApp.Client._Imports).Assembly);
+
+app.Run();
